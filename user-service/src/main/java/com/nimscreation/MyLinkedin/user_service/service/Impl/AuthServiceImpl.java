1c7d2b5568cda922f07814068526850e0da6444a
Nims-Creation <nimscreation06@gmail.com>
2026-03-24 18:04:30 +0530

    check point for signup user and update the yaml file

--- user-service/src/main/java/com/nimscreation/MyLinkedin/user_service/service/Impl/AuthServiceImpl.java
+++ user-service/src/main/java/com/nimscreation/MyLinkedin/user_service/service/Impl/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.nimscreation.MyLinkedin.user_service.dto.LoginRequestDto;
 import com.nimscreation.MyLinkedin.user_service.dto.SignupRequestDto;
 import com.nimscreation.MyLinkedin.user_service.dto.UserDto;
 import com.nimscreation.MyLinkedin.user_service.entity.User;
+import com.nimscreation.MyLinkedin.user_service.exception.BadRequestException;
 import com.nimscreation.MyLinkedin.user_service.exception.ResourceNotFoundException;
 import com.nimscreation.MyLinkedin.user_service.repository.UserRepository;
 import com.nimscreation.MyLinkedin.user_service.service.AuthService;
@@ -18,11 +19,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private UserRepository userRepository;
-    private ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final JwtServiceImpl jwtService;
 
     @Override
     public UserDto signup(SignupRequestDto signupRequestDto) {
+        boolean exists = userRepository.existsByEmail(signupRequestDto.getEmail());
+        if(exists){
+            throw new BadRequestException("User already exists, cannot signup again.");
+        }
         User user = modelMapper.map(signupRequestDto, User.class);
         user.setPassword(PasswordUtil.hashPassword(signupRequestDto.getPassword()));
 
@@ -35,6 +41,11 @@ public class AuthServiceImpl implements AuthService {
         User user = userRepository.findByEmail(loginRequestDto.getEmail())
                 .orElseThrow(()-> new ResourceNotFoundException("User not found with email:"+loginRequestDto.getEmail()));
 
+        boolean isPasswordMatch = PasswordUtil.checkPassword(loginRequestDto.getPassword(), user.getPassword());
 
+        if(!isPasswordMatch){
+            throw new BadRequestException("Incorrect password");
+        }
+        return jwtService.generateAccessToken(user);
     }
 }
